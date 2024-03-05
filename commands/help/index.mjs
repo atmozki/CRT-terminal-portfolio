ccb43d1dcf2733911c0d140574162e0cd93479cb
Dennis Jojo Kuriakose <62955521+atmozki@users.noreply.github.com>
2024-03-05 20:36:31 +0530

    Fixed typo in help command

--- commands/help/index.mjs
+++ commands/help/index.mjs
@@ -1,6 +1,6 @@
 const output = [
   "Available commands:",
-  "profile, skills, projects, contact, about, help, dino, hackerman, matrix, screensaver, cowsay, joke, fire, clear, reboot, quit",
+  "profile, skills, projects, contacts, about, help, dino, hackerman, matrix, screensaver, cowsay, joke, fire, clear, reboot, quit",
 ];
 
 export { output };
